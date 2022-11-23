74b32e24cc6bc26811c57e53bdd00b2133320da9
Francis Santiago <helper@sperocoin.org>
2022-11-23 17:04:12 -0300

    Aula 08

---
+++ 01-Variáveis/03-Verdadeiro_e_Falso/08-Negativa de Valores.py
@@ -0,0 +1,11 @@
+# PYTHON BÁSICO - 08 - Variáveis - Negativa de Valores
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Criado Por  : Francis Santiago
+# Data de Criação: 23/11/2022
+# version ='1.0.8'
+# ---------------------------------------------------------------------------
+ativa = True
+resultado = not ativa
+print(resultado)
