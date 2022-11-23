d60fd049a1c8fb5ca966f138bec2dd8fa32ee8c6
Francis Santiago <helper@sperocoin.org>
2022-11-23 16:52:15 -0300

    Aula 07

---
+++ 01-Variáveis/03-Verdadeiro_e_Falso/07-True_False.py
@@ -0,0 +1,10 @@
+# PYTHON BÁSICO - 07 - Variáveis - Verdadeiro e Falso(True / False)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Criado Por  : Francis Santiago
+# Data de Criação: 23/11/2022
+# version ='1.0.7'
+# ---------------------------------------------------------------------------
+ativo = False
+print(ativo)
