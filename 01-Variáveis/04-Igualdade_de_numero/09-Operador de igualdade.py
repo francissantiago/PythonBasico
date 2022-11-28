5700871393a375679c793c7216d53e8fde374a9c
Francis Santiago <helper@sperocoin.org>
2022-11-28 17:02:18 -0300

    Aula 09 - Operadores de igualdade

---
+++ 01-Variáveis/04-Igualdade_de_numero/09-Operador de igualdade.py
@@ -0,0 +1,12 @@
+# PYTHON BÁSICO - 09 - Variáveis - Operador de igualdade
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Criado Por  : Francis Santiago
+# Data de Criação: 28/11/2022
+# version ='1.0.9'
+# ---------------------------------------------------------------------------
+numero = 5
+outroNumero = 5
+retorno = numero == outroNumero
+print(retorno)
---
+++ 01-Variáveis/04-Igualdade_de_numero/10-Operador de desigualdade.py
@@ -0,0 +1,8 @@
+# PYTHON BÁSICO - 10 - Variáveis - Operador de desigualdade
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Criado Por  : Francis Santiago
+# Data de Criação: 28/11/2022
+# version ='1.1.0'
+# ---------------------------------------------------------------------------
